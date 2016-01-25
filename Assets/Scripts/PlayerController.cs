26267ced0e7b49f4f369633b096505cffd2df8f7
Lucas Pickering <pickering.l@husky.neu.edu>
2016-01-25 11:25:54 -0500

    Added other walls and rotation on wall collision

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,37 +4,63 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
 
+    private enum Rotation
+    {
+        up = 0, right = 90, down = 180, left = 270
+    };
+
     public int moveSpeed;
     public int jumpSpeed;
 
     private Rigidbody2D rigidBody;
-    private bool onGround = true;
+    private Rotation rotation;
+    private float distanceToGround;
 
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        distanceToGround = GetComponent<Collider2D>().bounds.extents.y;
     }
 
     void FixedUpdate()
     {
-        float lateralForce = Input.GetAxis("Horizontal") * moveSpeed;
-        float verticalForce = Input.GetButton("Vertical") && onGround ? jumpSpeed : 0f;
-        rigidBody.AddForce(new Vector2(lateralForce, verticalForce));
+        Vector2 force = new Vector2(0f, 0f);
+        if (CanJump())
+        {
+            force.x = Input.GetAxis("Horizontal") * moveSpeed;
+            force.y = Input.GetButton("Vertical") ? jumpSpeed : 0f;
+        }
+
+        force = Quaternion.Euler(new Vector3(0f, 0f, (float)rotation)) * force;
+        rigidBody.AddForce(force);
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag("Ground"))
+        switch (other.gameObject.tag)
         {
-            onGround = true;
+            case "Ground":
+                rotation = Rotation.up;
+                break;
+            case "Right Wall":
+                rotation = Rotation.right;
+                break;
+            case "Ceiling":
+                rotation = Rotation.down;
+                break;
+            case "Left Wall":
+                rotation = Rotation.left;
+                break;
+            default:
+                return;
         }
+
+        // We know a wall, ground, or ceiling was hit by now
+        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, (float)rotation));
     }
 
-    void OnCollisionExit2D(Collision2D other)
+    private bool CanJump()
     {
-        if (other.gameObject.CompareTag("Ground"))
-        {
-            onGround = false;
-        }
+        return Physics2D.Raycast(transform.position, transform.rotation * Vector3.down, distanceToGround + 0.1f);
     }
 }
