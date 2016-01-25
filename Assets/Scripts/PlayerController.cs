c414bd3981087a2939582b087c6b432ccb35032c
Lucas Pickering <pickering.l@husky.neu.edu>
2016-01-24 23:59:28 -0500

    Added player, working on movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public int moveSpeed;
+    public int jumpSpeed;
+
+    private Rigidbody2D rigidBody;
+    private bool onGround = true;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float lateralForce = Input.GetAxis("Horizontal") * moveSpeed;
+        float verticalForce = Input.GetButton("Vertical") && onGround ? jumpSpeed : 0f;
+        rigidBody.AddForce(new Vector2(lateralForce, verticalForce));
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            onGround = false;
+        }
+    }
+}
