3af574c7eb1d56b11a58785dddf77ac57d11b015
Lucas Pickering <pickering.l@husky.neu.edu>
2016-01-31 01:48:55 -0500

    Lots of initial stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,71 +1,93 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour
 {
 
-    private enum Rotation
+    private enum Orientation
     {
         up = 0, right = 90, down = 180, left = 270
     };
 
-    public int moveSpeed;
-    public int jumpSpeed;
+    private enum State
+    {
+        onGround, onWallCeil, inAir
+    };
+
+    static Dictionary<string, Orientation> orientationsBySurface = new Dictionary<string, Orientation>()
+    {
+        {"Ground", Orientation.up},
+        {"Left Wall", Orientation.right},
+        {"Ceiling", Orientation.down},
+        {"Right Wall", Orientation.left}
+    };
+
+    static Dictionary<Orientation, string> surfacesByOrientation = new Dictionary<Orientation, string>()
+    {
+        {Orientation.up, "Ground"},
+        {Orientation.right, "Left Wall"},
+        {Orientation.down, "Ceiling"},
+        {Orientation.left, "Right Wall"}
+    };
+
+    private const int TERRAIN_LAYER = 8;
+
+    public float groundMoveSpeed;
+    public float groundJumpSpeed;
+    public float wallCeilMoveSpeed;
+    public float wallCeilJumpSpeed;
 
     private Rigidbody2D rigidBody;
-    private Rotation rotation;
-    private float halfWidth;
-    private float distanceToGround;
+    private Orientation orientation;
+    private State state;
 
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
-
-        Collider2D collider = GetComponent<Collider2D>();
-        halfWidth = collider.bounds.extents.x;
-        distanceToGround = collider.bounds.extents.y;
     }
 
     void FixedUpdate()
     {
-        Vector2 force = new Vector2(0f, 0f);
-        if (CanJump())
+        float moveSpeed = 0f;
+        float jumpSpeed = 0f;
+        switch (state)
         {
-            force.x = Input.GetAxis("Horizontal") * moveSpeed;
-            force.y = Input.GetButton("Vertical") ? jumpSpeed : 0f;
+            case State.onGround:
+                moveSpeed = groundMoveSpeed;
+                jumpSpeed = groundJumpSpeed;
+                break;
+            case State.onWallCeil:
+                moveSpeed = wallCeilMoveSpeed;
+                jumpSpeed = wallCeilJumpSpeed;
+                break;
         }
-
-        rigidBody.AddRelativeForce(force);
+        rigidBody.AddRelativeForce(new Vector2(Input.GetAxis("Horizontal") * groundMoveSpeed,
+            Input.GetButton("Vertical") ? jumpSpeed : 0f));
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        switch (other.gameObject.tag)
+        if (other.gameObject.layer == TERRAIN_LAYER)
         {
-            case "Ground":
-                rotation = Rotation.up;
-                break;
-            case "Right Wall":
-                rotation = Rotation.right;
-                break;
-            case "Ceiling":
-                rotation = Rotation.down;
-                break;
-            case "Left Wall":
-                rotation = Rotation.left;
-                break;
-            default:
-                return;
+            string tag = other.gameObject.tag;
+            if (tag.Equals("Ground"))
+            {
+                state = State.onGround;
+            }
+            else
+            {
+                state = State.onWallCeil;
+            }
+            orientation = orientationsBySurface[other.gameObject.tag];
+            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, (float)orientation));
         }
-
-        // We know a wall, ground, or ceiling was hit by now
-        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, (float)rotation));
     }
 
-    private bool CanJump()
+    void OnCollisionExit2D(Collision2D other)
     {
-        // Raycast on the left and right edges of the bike
-        return Physics2D.Raycast(transform.position - new Vector3(halfWidth, 0f, 0f), transform.rotation * Vector3.down, distanceToGround + 0.1f) ||
-            Physics2D.Raycast(transform.position + new Vector3(halfWidth, 0f, 0f), transform.rotation * Vector3.down, distanceToGround + 0.1f);
+        if (other.gameObject.layer == TERRAIN_LAYER)
+        {
+            state = State.inAir;
+        }
     }
 }
