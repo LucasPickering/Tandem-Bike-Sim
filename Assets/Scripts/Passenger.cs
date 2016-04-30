3f9ba35f04c97120e5669b5f19df83c9fced40c2
Lucas Pickering <pickering.l@husky.neu.edu>
2016-04-30 15:33:38 -0400

    Passengers, other things

---
+++ Assets/Scripts/Passenger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Passenger : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.CompareTag ("Player")) {
+			SceneManager.LoadScene("Victory"); // Load the victory screen
+		}
+	}
+}
